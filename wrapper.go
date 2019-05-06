622d5083ea6279e121d0e3f61c07416ecd5edd75
SeukWon Kang <kasworld@gmail.com>
2019-05-06 16:31:15 +0900

    update from goguelike2

--- wrapper.go
+++ wrapper.go
@@ -1,4 +1,4 @@
-// Copyright 2017 SeukWon Kang (kasworld@gmail.com)
+// Copyright 2015,2016,2017,2018,2019 SeukWon Kang (kasworld@gmail.com)
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
@@ -8,61 +8,113 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
+
 package wrapper
 
 import (
 	"fmt"
-
-	"github.com/kasworld/wrapper/wrapperi"
+	"sync"
 )
 
-var _ wrapperi.WrapperI = &Wrapper{}
+// reuse same (witdh) wrapper
+var g_wrapper map[int]*Wrapper
+var g_mutex sync.Mutex
+
+func init() {
+	g_wrapper = make(map[int]*Wrapper)
+}
+
+func G_WrapperInfo() string {
+	return fmt.Sprintf("%v", g_wrapper)
+}
+
+func New(w int) *Wrapper {
+	g_mutex.Lock()
+	defer g_mutex.Unlock()
+
+	wr, exist := g_wrapper[w]
+	if exist {
+		return wr
+	}
+	wr = makeNew(w)
+	g_wrapper[w] = wr
+	return wr
+}
 
 type Wrapper struct {
 	wrapTable []int
 	width     int
+	widthMask int
+	Wrap      func(i int) int
+	WrapSafe  func(i int) int
 }
 
 func (wr Wrapper) String() string {
 	return fmt.Sprintf("Wrapper[%v]", wr.width)
 }
 
-func New(w int) *Wrapper {
-	wr := &Wrapper{
-		wrapTable: make([]int, w*3),
-		width:     w,
+func makeNew(w int) *Wrapper {
+	if w < 2 {
+		return nil
 	}
-	for i, _ := range wr.wrapTable {
-		wr.wrapTable[i] = i % w
+	if isPowerOfTwo(w) {
+		wr := &Wrapper{
+			width:     w,
+			widthMask: w - 1,
+		}
+		wr.Wrap = wr.wrapPowerOfTwo
+		wr.WrapSafe = wr.wrapPowerOfTwo
+		return wr
+	} else {
+		wr := &Wrapper{
+			wrapTable: make([]int, w*3),
+			width:     w,
+		}
+		for i, _ := range wr.wrapTable {
+			wr.wrapTable[i] = i % w
+		}
+		wr.Wrap = wr.wrapByTable
+		wr.WrapSafe = wr.wrapByCalc
+		return wr
 	}
-	return wr
+}
+
+func isPowerOfTwo(i int) bool {
+	return (i & (i - 1)) == 0
+}
+
+func (wr *Wrapper) GetWrapFn() func(i int) int {
+	return wr.Wrap
+}
+func (wr *Wrapper) GetWrapSafeFn() func(i int) int {
+	return wr.WrapSafe
 }
 
 func (wr *Wrapper) GetWidth() int {
 	return wr.width
 }
 
-func (wr *Wrapper) Wrap(i int) int {
-	return wr.wrapTable[i+wr.width]
+func (wr *Wrapper) IsIn(i int) bool {
+	return i >= 0 && i < wr.width
 }
 
-func (wr *Wrapper) WrapSafe(i int) int {
+func (wr *Wrapper) wrapByCalc(i int) int {
 	return (i%wr.width + wr.width) % wr.width
 }
 
-func (wr *Wrapper) WrapTable(i int) int {
+func (wr *Wrapper) wrapPowerOfTwo(i int) int {
+	return i & wr.widthMask
+}
+
+func (wr *Wrapper) wrapByTable(i int) int {
 	return wr.wrapTable[i+wr.width]
 }
 
-func (wr *Wrapper) WrapSimple(i int) int {
+func (wr *Wrapper) wrapSimple(i int) int {
 	return (i + wr.width) % wr.width
 }
 
-func (wr *Wrapper) WrapFull(i int) int {
-	return (i%wr.width + wr.width) % wr.width
-}
-
-func (wr *Wrapper) WrapIf(i int) int {
+func (wr *Wrapper) wrapIf(i int) int {
 	if i < 0 {
 		return i%wr.width + wr.width
 	} else if i >= wr.width {
@@ -72,7 +124,7 @@ func (wr *Wrapper) WrapIf(i int) int {
 	}
 }
 
-func (wr *Wrapper) WrapIfSimple(i int) int {
+func (wr *Wrapper) wrapIfSimple(i int) int {
 	if i < 0 {
 		return i + wr.width
 	} else if i >= wr.width {
--- wrapper_test.go
+++ wrapper_test.go
@@ -1,4 +1,4 @@
-// Copyright 2017 SeukWon Kang (kasworld@gmail.com)
+// Copyright 2015,2016,2017,2018,2019 SeukWon Kang (kasworld@gmail.com)
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
@@ -8,69 +8,94 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
+
 package wrapper
 
 import (
 	"testing"
 )
 
+// var wr *Wrapper
+
+// func TestMain(m *testing.M) {
+// 	wr = New(1000)
+// }
+
+func TestWrapper_WrapPowerOfTwo(t *testing.T) {
+	wr := New(1024)
+	for i := -wr.GetWidth() * 1; i < wr.GetWidth()*2; i++ {
+		if wr.Wrap(i) != wr.wrapByCalc(i) {
+			t.Fail()
+		}
+	}
+}
+
 func TestWrapper_Wrap(t *testing.T) {
-	wr := New(100)
-	for i := -100; i < 200; i++ {
-		t.Logf("%v %v", i, wr.Wrap(i))
-	}
-}
-
-func BenchmarkWrap(b *testing.B) {
-	wr := New(1000)
-	for i := 0; i < b.N; i++ {
-		for v := -wr.GetWidth(); v < wr.GetWidth()*2; v++ {
-			wr.Wrap(v)
+	wr := New(1023)
+	for i := -wr.GetWidth() * 1; i < wr.GetWidth()*2; i++ {
+		if wr.Wrap(i) != wr.wrapByCalc(i) {
+			t.Fail()
 		}
 	}
 }
 
-func BenchmarkWrapTable(b *testing.B) {
-	wr := New(1000)
+func wrapBench(b *testing.B, wr *Wrapper, wrfn func(i int) int) int {
+	sum := 0
 	for i := 0; i < b.N; i++ {
 		for v := -wr.GetWidth(); v < wr.GetWidth()*2; v++ {
-			wr.WrapTable(v)
+			sum += wrfn(v)
 		}
 	}
+	return sum
+}
+
+func BenchmarkWrap1023(b *testing.B) {
+	wr := New(1023)
+	wrapBench(b, wr, wr.Wrap)
+}
+func BenchmarkWrap1024(b *testing.B) {
+	wr := New(1024)
+	wrapBench(b, wr, wr.Wrap)
+}
+func BenchmarkGetWrapFn1023(b *testing.B) {
+	wr := New(1023)
+	wrapBench(b, wr, wr.GetWrapFn())
+}
+func BenchmarkGetWrapFn1024(b *testing.B) {
+	wr := New(1024)
+	wrapBench(b, wr, wr.GetWrapFn())
+}
+
+func BenchmarkWrapSafe(b *testing.B) {
+	wr := New(1023)
+	wrapBench(b, wr, wr.WrapSafe)
+}
+func BenchmarkWrapSafe1024(b *testing.B) {
+	wr := New(1024)
+	wrapBench(b, wr, wr.WrapSafe)
+}
+
+func BenchmarkWrapByTable(b *testing.B) {
+	wr := New(1023)
+	wrapBench(b, wr, wr.wrapByTable)
 }
 
 func BenchmarkWrapSimple(b *testing.B) {
-	wr := New(1000)
-	for i := 0; i < b.N; i++ {
-		for v := -wr.GetWidth(); v < wr.GetWidth()*2; v++ {
-			wr.WrapSimple(v)
-		}
-	}
+	wr := New(1023)
+	wrapBench(b, wr, wr.wrapSimple)
 }
 
-func BenchmarkWrapFull(b *testing.B) {
-	wr := New(1000)
-	for i := 0; i < b.N; i++ {
-		for v := -wr.GetWidth(); v < wr.GetWidth()*2; v++ {
-			wr.WrapFull(v)
-		}
-	}
+func BenchmarkWrapByCalc(b *testing.B) {
+	wr := New(1023)
+	wrapBench(b, wr, wr.wrapByCalc)
 }
 
 func BenchmarkWrapIf(b *testing.B) {
-	wr := New(1000)
-	for i := 0; i < b.N; i++ {
-		for v := -wr.GetWidth(); v < wr.GetWidth()*2; v++ {
-			wr.WrapIf(v)
-		}
-	}
+	wr := New(1023)
+	wrapBench(b, wr, wr.wrapIf)
 }
 
 func BenchmarkWrapIfSimple(b *testing.B) {
-	wr := New(1000)
-	for i := 0; i < b.N; i++ {
-		for v := -wr.GetWidth(); v < wr.GetWidth()*2; v++ {
-			wr.WrapIfSimple(v)
-		}
-	}
+	wr := New(1023)
+	wrapBench(b, wr, wr.wrapIfSimple)
 }
